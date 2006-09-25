cee0ad9532f5bf4fca959bb6667dec3e8a39c6ad
rram <rram@8d44d787-80f3-42f6-8d0b-07ebc01cdc8b>
2006-09-25 03:04:35 +0000

    # on 2006/09/25 03:04:35, techno did: Changed content type to application/octet-stream
    git-svn-id: file:///srv/svn/trunk/3m@497 8d44d787-80f3-42f6-8d0b-07ebc01cdc8b

--- dynamicmarkmast.php
+++ dynamicmarkmast.php
@@ -1,7 +1,7 @@
 <ASCII-WIN>
 <DefineCharStyle:Emphasis=<Nextstyle:Emphasis><cTypeface:Bold>><DefineCharStyle:Slant=<Nextstyle:Slant><cTypeface:Italic>><DefineCharStyle:EmphasisSlant=<Nextstyle:EmphasisSlant><cTypeface:Bold Italic>>
 <?php
-header('Content-type: text/plain');
+header('Content-type: application/octet-stream');
 include 'config.php';
 // These define a bunch of variables that should be changed if the format of the mast changes
 $nameSeperator=",";
