60d14594318c6258c8d129d4120892816bac190b
rram <rram@8d44d787-80f3-42f6-8d0b-07ebc01cdc8b>
2007-11-28 16:42:10 +0000

    # on 2007/11/28 16:42:10, rram did: Moved inclusion of config to beginning for fail-fast behavior.
    git-svn-id: file:///srv/svn/trunk/3m@514 8d44d787-80f3-42f6-8d0b-07ebc01cdc8b

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php include 'config.php'; ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
@@ -20,7 +20,6 @@
     <td colspan="4"><a href="createform.php">Create New User</a></td>
   </tr>
   <?php
-include 'config.php';
 $query="SELECT `last`, `first`, `middle`, `email` FROM `bio` GROUP BY `email` ORDER BY `last`";
 $result=mysqlquery($dbnames,$query);
 $num=mysql_numrows($result);
