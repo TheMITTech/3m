6475336195df82db335b611ebd2fb1982b477646
rram <rram@8d44d787-80f3-42f6-8d0b-07ebc01cdc8b>
2008-06-23 17:01:29 +0000

    # on 2008/06/23 17:01:29, rram did: Manboard voters e-mail list.
    git-svn-id: file:///srv/svn/trunk/3m@530 8d44d787-80f3-42f6-8d0b-07ebc01cdc8b

---
+++ contactlists/manboardvotersmail.php
@@ -0,0 +1,24 @@
+<?php
+include '../config.php';
+?>
+<html>
+<body>
+<table>
+<?php
+$query="SELECT * FROM bio WHERE active=1 ORDER BY email";
+$result=mysqlquery($dbnames,$query);
+$num=mysql_numrows($result);
+for ($i=0; $i < $num; $i++) {
+$dept=mysql_result($result,$i,"dept");
+$position=mysql_result($result,$i,"position");
+$email=mysql_result($result,$i,"email");
+$test1=substr($position,0,9);
+$test2=($test1!="Associate");
+if ((substr($position,0,9)!="Associate")&&(substr($position,0,6)!="Senior")&&((substr($position,-7,7)=="Manager")||(substr($position,-6,6)=="Editor")||($position=="Editor in Chief")||($position=="Chairman")||($position=="Director"))){
+echo "<tr><td>$email</td></tr>";
+}
+}
+?>
+</table>
+</body>
+</html>
