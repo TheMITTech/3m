c053204bdc55846b60ee3d66c618240a2fa1f933
William T Navarre <navarre@the-tech.mit.edu>
2016-03-08 16:56:44 -0500

    Merged SHEMA files into one.

--- DATABASE_SCHEMA.sql
+++ DATABASE_SCHEMA.sql
@@ -55,6 +55,30 @@ LOCK TABLES `staff` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table sructure for table `emailrules` and `previousemailupdate`
+--
+-- Written by hand. 
+SET @saved_cs_client = @@character_set_client;
+SET character_set_client = utf8;
+DROP TABLE IF EXISTS emailrules;
+CREATE TABLE `emailrules` (
+       `ruleid` int unsigned NOT NULL auto_increment,
+       `dept` varchar(20) collate utf8_unicode_ci NOT NULL default '',
+       `position` varchar(30) collate utf8_unicode_ci NOT NULL,
+       `addlist` varchar(150) collate utf8_unicode_ci NOT NULL,
+       `notificationlist` varchar(35) collate utf8_unicode_ci NOT NULL,
+       PRIMARY KEY (`ruleid`)
+       ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS previousemailupdate;
+CREATE TABLE `previousemailupdate` (
+       `updateid` int unsigned NOT NULL auto_increment,
+       `addlist` varchar(40) collate utf8_unicode_ci NOT NULL,
+       `athena_username` varchar(8) collate utf8_unicode_ci NOT NULL,
+       PRIMARY KEY (`updateid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
 -- Table structure for table `departments`
 --
 
